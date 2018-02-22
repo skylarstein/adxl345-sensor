ba4edab12c9d822d1d4999a1f26f4a7c3c4277f6
Andrew Bythell <abythell@ieee.org>
2018-02-21 20:33:07 -0800

    add jsdoc

--- ADXL345.js
+++ ADXL345.js
@@ -1,15 +1,19 @@
-/*
-  ADXL345.js
-
-  A Node.js I2C module for the Analog Devices ADXL345 three-axis digital accelerometer.
-*/
-
 'use strict';
 
-let i2c = require('i2c-bus');
+const i2c = require('i2c-bus');
 
+/**
+*  A Node.js I2C module for the Analog Devices ADXL345 three-axis digital accelerometer.
+*/
 class ADXL345 {
 
+  /**
+   * @constructor
+   * @param {Object} [options]
+   * @param {number} [options.i2cBusNo=1] - i2c bus number, ie. /dev/i2c-X
+   * @param {number} [options.i2cAddress=0x53] - device i2c address.
+   * @param {Object} [options.i2cBus] - i2c-bus Bus object (inject for testing).
+   */
   constructor(options) {
     this.i2cBusNo = (options && options.hasOwnProperty('i2cBusNo')) ? options.i2cBusNo : 1;
     this.i2cAddress = (options && options.hasOwnProperty('i2cAddress')) ? options.i2cAddress : ADXL345.I2C_ADDRESS_ALT_GROUNDED();
@@ -33,6 +37,14 @@ class ADXL345 {
     this.EARTH_GRAVITY_MS2 = 9.80665;
   }
 
+  /**
+   * Write a byte to an ADXL345 register.  Abstracts and Promisifies
+   * i2c writes.
+   * @private
+   * @param {number} register
+   * @param {number} value - 8-bit value.
+   * @returns {Promise} Resolves on success, rejects with Error.
+   */
   writeByte(register, value) {
     return new Promise((resolve, reject) => {
       this.i2cBus.writeByte(this.i2cAddress, register, value, (err) => {
@@ -46,40 +58,59 @@ class ADXL345 {
     });
   }
 
+  /**
+   * Read a byte from an ADXL345 register.  Abstracts and Promisifies
+   * i2c reads.
+   * @private
+   * @param {number} register
+   * @returns {Promise} Resolves with 8-bit register value, rejects with Error.
+   */
   readByte(register) {
     return new Promise((resolve, reject) => {
       this.i2cBus.readByte(this.i2cAddress, register, (err, value) => {
-        if (err) {
-          reject(err);
-        }
-        else {
-          resolve(value);
-        }
+        return err ? reject(err) : resolve(value);
       });
     });
   }
 
+  /**
+   * Read bytes from a series of sequential ADXL345 registers.  Abstracts around
+   * Promisifies i2c block reads.
+   * @private
+   * @param {number} register - Starting register address.
+   * @param {number} bytes - Number of bytes to read.
+   * @returns {Promise} Resolves with Buffer[bytes], rejects with Error.
+   */
   readBlock(register, bytes) {
     return new Promise((resolve, reject) => {
       this.i2cBus.readI2cBlock(this.i2cAddress, register, bytes, new Buffer(bytes), (err, bytesRead, buffer) => {
-        if (err) {
-          reject(err);
-        }
-        else {
-          resolve(buffer);
-        }
+        return err ? reject(err) : resolve(buffer);
       });
     });
   }
 
+  /**
+   * Read Device ID
+   * @returns {Promise} Resolves with device ID, rejects with Error.
+   */
   getDevId() {
     return this.readByte(this.ADXL345_REG_DEVID);
   }
 
+  /**
+   * Set POWER_CTL register.
+   * @param {number} value - 8-bit register value
+   * @returns {Promise} Resolves on success, rejects with Error.
+   */
   setPowerCtl(value) {
     return this.writeByte(this.ADXL345ADXL345_REG_POWER_CTL, value);
   }
 
+  /**
+   * Initialize the device.  Performs a device ID check, then  puts then
+   * device into measurement mode.
+   * @returns {Promise} Resolves with device ID on success, rejects with Error.
+   */
   init() {
     return this.getDevId().then((deviceId) => {
       if (deviceId !== ADXL345.DEVICE_ID()) {
@@ -90,6 +121,12 @@ class ADXL345 {
     });
   }
 
+  /**
+   * Read acceleration data for X,Y,Z axis.
+   * @param {boolean} [gForce=false] - When true, return values in g, when
+   * false, return values in m/s/s.
+   * @returns {Promise} Resolves with {x, y, z, units}
+   */
   getAcceleration(gForce) {
     return this.readBlock(this.ADXL345_REG_DATAX0, 6).then((buffer) => {
       let x = this.int16(buffer[1], buffer[0]) * this.ADXL345_MG2G_SCALE_FACTOR;
@@ -104,6 +141,12 @@ class ADXL345 {
     });
   }
 
+  /**
+   * Set measurement range.  Regarless of range, full-resolution mode is
+   * always used.
+   * @param {number} range - ADXL345.RANGE_X_G() | X=2,4,8,16
+   * @returns {Promise} Resolves on success, rejects with Error.
+   */
   setMeasurementRange(range) {
     if(!ADXL345.isValidRange(range)) {
       return Promise.reject(`Invalid range (${range})`);
@@ -116,44 +159,67 @@ class ADXL345 {
     });
   }
 
+  /**
+   * Get measurement range.
+   * @returns {Promise} Resolves with 2-bit range value, rejects on Error.  Result
+   * can be converted to a string using {@link stringifyMeasurementRange}.
+   */
   getMeasurementRange() {
     return this.readByte(this.ADXL345_REG_DATA_FORMAT).then((format) => format & 0b11);
   }
 
+  /**
+   * Set date rate.  LOW_POWER bit is always cleared (ie. normal operation)
+   * when the rate is set.
+   * @param {number} dataRate - 4-bit data rate value
+   * @returns {Promise} Resolves on success, rejects with Error.
+   */
   setDataRate(dataRate) {
     if(!ADXL345.isValidDataRate(dataRate)) {
       return Promise.reject(Error(`Invalid data rate (${dataRate})`));
     }
-    // We'll always clear the LOW_POWER bit and the remaining MSBs are unused,
-    // so no need to read ADXL345_REG_BW_RATE before writing.
-    //
     return this.writeByte(this.ADXL345_REG_BW_RATE, dataRate & 0b1111);
   }
 
+  /**
+   * Get data rate
+   * @returns {Promise} Resolves with 4-bit data rate value, rejects with Error.
+   */
   getDataRate() {
     return this.readByte(this.ADXL345_REG_BW_RATE).then((bwRate) => (bwRate & 0b1111));
   }
 
-  /* The OFSX, OFSY, and OFSZ registers are each eight bits and offer user-set offset
-     adjustments in twos complement format with a scale factor of 15.6 mg/LSB
-     (that is, 0x01 = 0.0156 g, 0x7F = 2 g). The value stored in the offset registers
-     is automatically added to the acceleration data, and the resulting value is
-     stored in the output data registers. For additional information regarding offset
-     calibration and the use of the offset registers, refer to the Offset Calibration
-     section in the ADXL345 datasheet.
-  */
+  /**
+   * Set the X-axis offset value.
+   * @param {number} value - 8-bits, twos compliment, 15.6mg/LSB.
+   * @returns {Promise} Resolves on success, rejects with Error.
+   */
   setOffsetX(value) {
     return this.writeByte(this.ADXL345_REG_OFSX, value);
   }
 
+  /**
+   * Set the Y-axis offset value.
+   * @param {number} value - 8-bits, twos compliment, 15.6mg/LSB.
+   * @returns {Promise} Resolves on success, rejects with Error.
+   */
   setOffsetY(value) {
     return this.writeByte(this.ADXL345_REG_OFSY, value);
   }
 
+  /**
+   * Set the Z-axis offset value.
+   * @param {number} value - 8-bits, twos compliment, 15.6mg/LSB.
+   * @returns {Promise} Resolves on success, rejects with Error.
+   */
   setOffsetZ(value) {
     return this.writeByte(this.ADXL345_REG_OFSZ, value);
   }
 
+  /**
+   * Read offset settings.  One call reads all 3 offset registers.
+   * @returns {Promise} Resolves with {x, y, z}, rejects with Error.
+   */
   getOffsets() {
     return this.readBlock(this.ADXL345_REG_OFSX, 3).then((buffer) => ({
         x : buffer[0],
@@ -162,15 +228,35 @@ class ADXL345 {
       }));
   }
 
+  /**
+   * Convert 2-byte array into unsigned 16-bit integer
+   * @private
+   * @param {number} msb
+   * @param {number} lsb
+   * @returns {number} 16-bit unsigned integer.
+   */
   uint16(msb, lsb) {
     return msb << 8 | lsb;
   }
 
+  /**
+   * Convert 2-byte array into signed 16-bit integer
+   * @private
+   * @param {number} msb
+   * @param {number} lsb
+   * @returns {number} 16-bit signed integer.
+   */
   int16(msb, lsb) {
     let val = this.uint16(msb, lsb);
     return val > 32767 ? (val - 65536) : val;
   }
 
+  /**
+   * Verify range bits are valid
+   * @private
+   * @param {number} range - 2-bit range value.
+   * @returns {boolean} true if valid.
+   */
   static isValidRange(range) {
     switch(range) {
       // Could simply check for range >= 0 && range <= 0x3 but to be padantic...
@@ -185,6 +271,12 @@ class ADXL345 {
     }
   }
 
+  /**
+  * Verify data rate bits are valid.
+  * @private
+  * @param {number} dataRate - 2-byte range value
+  * @returns {boolean} true if valid.
+  */
   static isValidDataRate(dataRate) {
     switch(dataRate) {
       // Could simply check for dataData >= 0 && dataRate <= 0xF but to be padantic...
@@ -211,6 +303,11 @@ class ADXL345 {
     }
   }
 
+  /**
+   * Convert range bits into string.
+   * @param {number} range - 2-bit range value
+   * @returns {string}
+   */
   static stringifyMeasurementRange(range) {
     switch(range) {
       case ADXL345.RANGE_2_G() : return 'RANGE_2_G';
@@ -223,6 +320,11 @@ class ADXL345 {
     }
   }
 
+  /**
+   * Convert rate bytes into string.
+   * @param {number} rate - 2-byte rate value
+   * @returns {string}
+   */
   static stringifyDataRate(dataRate) {
     switch(dataRate) {
       case ADXL345.DATARATE_0_10_HZ() : return 'DATARATE_0_10_HZ';
